5d7524bb55a23bd03e614cf65c5b66cec6ada089
51m0n397 <simbartolini@gmail.com>
2021-01-26 12:02:47 +0100

    Added globalstats leaderboard

---
+++ MyProject/app/src/main/java/it/simone/myproject/LeaderboardAdapter.kt
@@ -0,0 +1,28 @@
+package it.simone.myproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import it.simone.myproject.globalstats.model.Player
+import kotlinx.android.synthetic.main.item_leaderboard.view.*
+
+class LeaderboardAdapter(private val list: List<Player>) : RecyclerView.Adapter<LeaderboardAdapter.ViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_leaderboard, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.view_rank.text = list[position].rank.toString()
+        holder.itemView.view_name.text = list[position].name
+        holder.itemView.view_score.text = list[position].value.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
+}
--- MyProject/app/src/main/java/it/simone/myproject/LeaderboardFragment.kt
+++ MyProject/app/src/main/java/it/simone/myproject/LeaderboardFragment.kt
@@ -1,11 +1,12 @@
 package it.simone.myproject
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import it.simone.myproject.LoginFragment.Companion.globalstatsId
 import it.simone.myproject.globalstats.api.GlobalstatsApi
 import kotlinx.android.synthetic.main.fragment_leaderboard.*
@@ -27,12 +28,25 @@ class LeaderboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val context = this.requireContext()
+
         GlobalScope.launch {
-            val score = GlobalstatsApi().getUserStatistic(globalstatsId)?.value
+            val stats = GlobalstatsApi().getUserStatistic(globalstatsId)
+            val leaderboard = GlobalstatsApi().getLeaderboard(100)
 
             withContext(Dispatchers.Main) {
-                view_my_highest_score.text = getString(R.string.my_highest_score) + " " + score
+                view_my_highest_score.text = getString(R.string.my_highest_score) + " " + stats?.value
+                view_my_rank.text = getString(R.string.my_rank) + " " + stats?.rank
+                val itemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+                view_leaderboard_list.addItemDecoration(itemDecoration)
+
+                val adapter = LeaderboardAdapter(leaderboard)
+
+                view_leaderboard_list.layoutManager = LinearLayoutManager(context)
+                view_leaderboard_list.adapter = adapter
             }
         }
+
+
     }
 }
